88d1a28406397266cc5c595e3475b5d2b8e0301e
root <root@ip-172-31-10-58.ap-south-1.compute.internal>
2024-09-19 08:41:16 +0000

    spring

---
+++ mi-spring/src/main/java/com/motivity/hospital/config/WebConfig.java
@@ -0,0 +1,46 @@
+package com.motivity.hospital.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig {
+    private static final int CORS_FILTER_ORDER = -102;
+    @Value("${domain.name}")
+    private String domain;
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+
+        String s="as";
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(domain);
+//        config.addAllowedOrigin("*");
+//        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedMethods(Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name()));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+
+        // should be set order to -100 because we need to CorsFilter before SpringSecurityFilter
+        bean.setOrder(CORS_FILTER_ORDER);
+        return bean;
+    }
+}
